24dc3a9d1d746812318e33b3d1d49a01de92302b
SaadAhmadSaddiqui <mehunsaadi@gmail.com>
2024-05-19 12:57:17 +0400

    console logging the result of the serp api result

--- server/flights.js
+++ server/flights.js
@@ -1,18 +1,18 @@
-import { getJson } from "serpapi"
+import { getJson } from "serpapi";
 import { API_KEY } from "./constants.js";
 
-export const fetchFlightData = async (departureId, arrivalId, outboundDate, returnDate, currency, type, tripType) => {
-    const res = await getJson({
-      engine: 'google_flights',
-      departure_id: departureId,
-      arrival_id: arrivalId,
-      outbound_date: outboundDate,
-      return_date: returnDate,
-      currency: currency,
-      hl: 'en',
-      api_key: API_KEY,
-      type: type || 2,
-    }) 
-    
-    return res;
-}
+export const fetchFlightData = async (departureId, arrivalId, outboundDate, returnDate, currency) => {
+  const res = await getJson({
+    engine: "google_flights",
+    departure_id: departureId,
+    arrival_id: arrivalId,
+    outbound_date: outboundDate,
+    return_date: returnDate,
+    currency: currency,
+    hl: "en",
+    api_key: API_KEY,
+    type: 1,
+  });
+
+  return res;
+};
--- server/index.js
+++ server/index.js
@@ -8,23 +8,22 @@ config.api_key = API_KEY;
 
 const app = express();
 
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: "*" }));
 app.use(json());
 
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, () => {
-console.log(`Server listening on port ${PORT}\n-------------------------`);
+  console.log(`Server listening on port ${PORT}\n-------------------------`);
 });
 
-app.post('/flights', async (req, res) => {
-  const { departureId, arrivalId, outboundDate, returnDate, currency, type, tripType} = req.body;
+app.post("/flights", async (req, res) => {
+  const { departureId, arrivalId, outboundDate, returnDate, currency } = req.body;
   try {
-    const result = await fetchFlightData(departureId, arrivalId, outboundDate, returnDate, currency, type, tripType);
+    const result = await fetchFlightData(departureId, arrivalId, outboundDate, returnDate, currency);
     return res.json(result).end();
   } catch (error) {
-    console.error(error)
-    return res.status(500).json({error}).end();
+    console.error(error);
+    return res.status(500).json({ error }).end();
   }
 });
